22f5740ac73ac481e4cf226cdb1baf977a256318
mayor <david.senack@gmail.com>
2026-01-22 10:17:59 -0500

    Fix homebrew formula SHA256

--- packaging/homebrew/gastop.rb
+++ packaging/homebrew/gastop.rb
@@ -6,7 +6,7 @@ class Gastop < Formula
   desc "htop-like terminal UI for Gas Town workspaces"
   homepage "https://github.com/davidsenack/gastop"
   url "https://github.com/davidsenack/gastop/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "7cef48c21801f8c11a7e5fb071700998ac83032651799a1be5e60a8f78598c1e"
   license "MIT"
   head "https://github.com/davidsenack/gastop.git", branch: "main"
 
